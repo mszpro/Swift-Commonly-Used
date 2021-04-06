159eb7255b7afdc98ca0a1c9c222e951015e5a48
シュンジョーァ <68307970+mszpro@users.noreply.github.com>
2021-04-05 21:38:41 -0400

    Update SwiftUI-View.swift

--- SwiftUI-View.swift
+++ SwiftUI-View.swift
@@ -1,3 +1,15 @@
 // Dismiss a SwiftUI view within the view
 @Environment(\.presentationMode) var presentationMode
 presentationMode.wrappedValue.dismiss()
+
+// Use a SwiftUI view (InfoView) in UIStoryBoard
+class InfoViewHostingController: UIHostingController<InfoView> {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder,rootView: InfoView());
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
